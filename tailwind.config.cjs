6a26336f73b2d39d1645d8a11ea4f8d2e3ff393a
alexis00rod <alexis00rodrigo@gmail.com>
2023-01-27 23:38:08 -0300

    Agregue google fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,12 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+          'poppins': ['"Poppins"', 'sans-serif'],
+          'monospace': ['"Space Mono"', 'monospace'],
+      },
+  },
   },
   plugins: [],
 }
