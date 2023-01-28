281e7c65adf39ce7a1a0cfa899e17b973bd80aab
alexis00rod <alexis00rodrigo@gmail.com>
2023-01-27 23:52:59 -0300

    Agregue paginas principales

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import { ConvertLength } from "./ConvertLength/ConvertLength";
+import { ConvertTemperature } from "./ConvertTemperature/ConvertTemperature";
+import { ConvertTime } from "./ConvertTime/ConvertTime";
+import { ConvertWeight } from "./ConvertWeight/ConvertWeight";
+import { Home } from "./Home/Home";
+import { Translate } from "./Translate/Translate";
+
+export {Home,ConvertWeight,ConvertLength,ConvertTime,ConvertTemperature,Translate}
---
+++ src/pages/pages.js
@@ -0,0 +1,7 @@
+export const pages = [
+  {id:"convert-weight",title:"Conversion de peso",icon:"scale-balanced"},
+  {id:"convert-length",title:"Conversion de longitud",icon:"ruler"},
+  {id:"convert-time",title:"Conversion de tiempo",icon:"hourglass-start"},
+  {id:"convert-temperature",title:"Conversion de temperatura",icon:"temperature-three-quarters"},
+  {id:"translate",title:"Traductor",icon:"language"},
+]
