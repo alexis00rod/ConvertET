272d96186890723c8ad0eaaf1100ec511cb5c0e1
alexis00rod <alexis00rodrigo@gmail.com>
2023-01-27 23:51:35 -0300

    Configure rutas

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,21 @@
+import { Route, Routes } from "react-router-dom"
+import { Navbar, Layout } from '../'
+import { ConvertLength, ConvertTemperature, ConvertTime, ConvertWeight, Home, Translate } from "../../pages"
+
 export const App = () => {
   return (
-    <div>App</div>
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}></Route>
+          <Route path="convert-weight" element={<ConvertWeight />}></Route>
+          <Route path="convert-length" element={<ConvertLength />}></Route>
+          <Route path="convert-time" element={<ConvertTime />}></Route>
+          <Route path="convert-temperature" element={<ConvertTemperature />}></Route>
+          <Route path="translate" element={<Translate />}></Route>
+        </Route>
+      </Routes>
+    </div>
   )
 }
